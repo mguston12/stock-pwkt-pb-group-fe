76bd9eef2026515bd7267347ea250fb8c1f6dd61
michael <michael12guston@gmail.com>
2025-05-23 18:17:17 +0700

    update: mesin page

--- src/views/pages/machine/Machine.js
+++ src/views/pages/machine/Machine.js
@@ -10,6 +10,8 @@ import {
   CModal,
   CModalBody,
   CModalFooter,
+  CModalHeader,
+  CModalTitle,
   CPagination,
   CPaginationItem,
   CRow,
@@ -23,6 +25,8 @@ import {
 } from '@coreui/react'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
+import CIcon from '@coreui/icons-react'
+import { cilCheckCircle, cilXCircle } from '@coreui/icons'
 
 const Machine = () => {
   const [listMachine, setListMachine] = useState([])
@@ -31,8 +35,16 @@ const Machine = () => {
   const [selectedCompany, setSelectedCompany] = useState(null)
   const [currentPage, setCurrentPage] = useState(1)
   const [totalPage, setTotalPage] = useState(1)
+  const [showReplaceModal, setShowReplaceModal] = useState(false)
+  const [selectedMachineId, setSelectedMachineId] = useState(null)
+  const [newMachineId, setNewMachineId] = useState('')
+  const [showDeactivateModal, setShowDeactivateModal] = useState(false)
   const itemsPerPage = 5
   const maxVisiblePages = 3
+
+  const [modalResponseIsOpen, setModalResponseIsOpen] = useState(false)
+  const [responseMessage, setResponseMessage] = useState('')
+  const [responseType, setResponseType] = useState(false)
 
   useEffect(() => {
     GetMachines()
@@ -87,6 +99,28 @@ const Machine = () => {
     }
   }
 
+  const handleReplaceSubmit = () => {
+    axios
+      .post('http://192.168.88.250:8081/machines/replace', {
+        old_machine_id: selectedMachineId,
+        new_machine_id: newMachineId,
+      })
+      .then((response) => {
+        // Tanggapi respons dari server
+        setResponseType(true)
+        setResponseMessage('Berhasil Tukar Mesin')
+        setModalResponseIsOpen(true)
+        setShowDeactivateModal(false)
+        GetMachines() // Memuat ulang daftar mesin
+      })
+      .catch((error) => {
+        setIsLoading(false)
+        setModalResponseIsOpen(true)
+        setResponseType(false)
+        setResponseMessage(error.message)
+      })
+  }
+
   const handlePageChange = (newPage) => {
     if (newPage < 1 || newPage > totalPage) return // Prevent out-of-bounds page numbers
     setCurrentPage(newPage)
@@ -111,6 +145,74 @@ const Machine = () => {
     }
 
     return pages
+  }
+
+  const handleReplace = (machineId) => {
+    // Tampilkan modal untuk mengganti mesin
+    setSelectedMachineId(machineId)
+    setShowReplaceModal(true)
+  }
+  const handleDeactivate = (machineId) => {
+    // Tampilkan modal konfirmasi untuk menonaktifkan mesin
+    setSelectedMachineId(machineId)
+    setShowDeactivateModal(true)
+  }
+
+  const handleDeactivateSubmit = () => {
+    axios
+      .post('http://192.168.88.250:8081/machines/deactivate', {
+        machine_id: selectedMachineId,
+      })
+      .then((response) => {
+        // Tanggapi respons dari server
+
+        setResponseType(true)
+        setResponseMessage('Berhasil Membuat Request Baru')
+        setModalResponseIsOpen(true)
+        setShowDeactivateModal(false)
+        GetMachines() // Memuat ulang daftar mesin
+      })
+      .catch((error) => {
+        setIsLoading(false)
+        setModalResponseIsOpen(true)
+        setResponseType(false)
+        setResponseMessage(error.message)
+      })
+  }
+
+  function activateMachine() {
+    setIsLoading(true)
+    var obj = {
+      id_machine: selectedMachineId,
+      id_customer: selectedCustomer,
+      tanggal_mulai: tanggalMulai,
+    }
+    var url = `http://192.168.88.250:8081/spareparts/update`
+
+    axios
+      .put(url, obj)
+      .then((response) => {
+        SearchSparepart()
+        if (response.data.error.status === true) {
+          console.log('Gagal Update Sparepart', response)
+          setIsLoading(false)
+          setResponseType(false)
+          setResponseMessage(response.data.error.msg)
+          setModalResponseIsOpen(true)
+        } else {
+          setIsLoading(false)
+          console.log('Berhasil Update Sparepart', response)
+          setResponseType(true)
+          setResponseMessage('Berhasil Update Sparepart')
+          setModalResponseIsOpen(true)
+        }
+      })
+      .catch((error) => {
+        setIsLoading(false)
+        setModalResponseIsOpen(true)
+        setResponseType(false)
+        setResponseMessage(error.message)
+      })
   }
 
   return (
@@ -163,6 +265,20 @@ const Machine = () => {
                     <CTableDataCell>{item.tipe_machine}</CTableDataCell>
                     <CTableDataCell>{item.id_customer}</CTableDataCell>
                     <CTableDataCell>
+                      <CButton
+                        className="btn btn-danger btn-sm text-white"
+                        style={{ marginRight: '5px' }}
+                        onClick={() => handleDeactivate(item.id_machine)}
+                      >
+                        Nonaktifkan
+                      </CButton>
+                      <CButton
+                        className="btn btn-warning btn-sm text-white"
+                        style={{ marginRight: '5px' }}
+                        onClick={() => handleReplace(item.id_machine)}
+                      >
+                        Ganti Mesin
+                      </CButton>
                       <Link
                         to={`/machine/detail/${item.id_machine}`}
                         className="btn btn-primary btn-sm text-white"
@@ -235,6 +351,77 @@ const Machine = () => {
         </CModalFooter>
       </CModal>
       {/* MODAL LOADING */}
+      <CModal
+        visible={showReplaceModal}
+        onClose={() => setShowReplaceModal(false)}
+        size="lg"
+        alignment="center"
+      >
+        <CModalHeader>
+          <CModalTitle>Ganti Mesin</CModalTitle>
+        </CModalHeader>
+        <CModalBody>
+          {/* Form untuk memilih mesin baru */}
+          <CFormInput
+            type="text"
+            placeholder="ID Mesin Baru"
+            value={newMachineId}
+            onChange={(e) => setNewMachineId(e.target.value)}
+          />
+        </CModalBody>
+        <CModalFooter>
+          <CButton color="secondary" onClick={() => setShowReplaceModal(false)}>
+            Batal
+          </CButton>
+          <CButton color="primary" onClick={handleReplaceSubmit}>
+            Ganti Mesin
+          </CButton>
+        </CModalFooter>
+      </CModal>
+
+      <CModal
+        size="sm"
+        alignment="center"
+        visible={showDeactivateModal}
+        onClose={() => setShowDeactivateModal(false)}
+      >
+        <CModalBody>Apakah Anda yakin ingin menonaktifkan mesin ini?</CModalBody>
+        <CModalFooter>
+          <CButton color="secondary" onClick={() => setShowDeactivateModal(false)}>
+            Batal
+          </CButton>
+          <CButton color="danger" onClick={handleDeactivateSubmit}>
+            Nonaktifkan
+          </CButton>
+        </CModalFooter>
+      </CModal>
+      {/* MODAL RESPONSE*/}
+      <CModal size="md" alignment="center" visible={modalResponseIsOpen} backdrop="static">
+        <CModalBody style={{ justifyContent: 'center', textAlign: 'center' }}>
+          <CFormLabel style={{ fontWeight: 'bold', fontSize: '1rem' }}>
+            {responseMessage}
+          </CFormLabel>
+          <br />
+          {responseType && (
+            <CIcon
+              icon={cilCheckCircle}
+              style={{ color: 'green', width: '5rem', height: '5rem' }}
+            />
+          )}
+          {!responseType && (
+            <CIcon icon={cilXCircle} style={{ color: 'red', width: '3rem', height: '3rem' }} />
+          )}
+        </CModalBody>
+        <CModalFooter style={{ justifyContent: 'center' }}>
+          <Link to={`/request`} className="btn btn-block btn-info text-white">
+            Kembali Ke Halaman Request
+          </Link>
+          <CButton color="success" className="text-white" onClick={() => window.location.reload()}>
+            Tambah Request Lagi
+          </CButton>
+        </CModalFooter>
+      </CModal>
+      {/* MODAL RESPONSE */}
     </CCard>
   )
 }
